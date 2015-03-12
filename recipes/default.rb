d422c8fc9d8bc19372d50c7a59502707e2ce0852
Stoned Elipot <stoned.elipot@gmail.com>
2015-03-12 01:01:52 +0100

    use omnibus build user's environment

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,10 +2,18 @@ logdir = node['omnibus']['build_dir'] + '/.kitchen/logs'
 
 execute 'bundle install' do
   cwd node['omnibus']['build_dir']
-  command "bundle install > #{logdir}/#{node['hostname']}-bundle-install.log 2>&1"
+  command <<-EOF.gsub(/\s+/, ' ').strip!
+    sudo -i -u #{node['omnibus']['build_user']} \
+    bash -l -c 'cd #{node['omnibus']['build_dir']}; \
+    bundle install > #{logdir}/#{node['hostname']}-bundle-install.log 2>&1'
+    EOF
 end
 
 execute 'build project' do
   cwd node['omnibus']['build_dir']
-  command "bundle exec omnibus build #{node['omnibus']['build_project']} > #{logdir}/#{node['hostname']}-build-project.log 2>&1"
+  command <<-EOF.gsub(/\s+/, ' ').strip!
+    sudo -i -u #{node['omnibus']['build_user']} \
+    bash -l -c 'cd #{node['omnibus']['build_dir']}; \
+    bundle exec omnibus build #{node['omnibus']['build_project']} > #{logdir}/#{node['hostname']}-build-project.log 2>&1'
+    EOF
 end

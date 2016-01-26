114542c8a6ef65a80fb32a98d3740907adc34971
Stoned Elipot <stoned.elipot@gmail.com>
2016-01-26 07:23:43 +0100

    Accommodate with omnibus cookbook 3.0.0 and up
    Omnibus cookbook 3.0.0 and up does not set the build user's PATH
    with its ruby installation path anymore. So load the build user's
    load-omnibus-toolchain.sh script to set the PATH thus to be able
    the find ruby and associated tools.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,9 @@ execute 'bundle install' do
   cwd node['omnibus']['build_dir']
   command <<-EOF.gsub(/\s+/, ' ').strip!
     sudo -i -u #{node['omnibus']['build_user']} \
-    bash -l -c 'cd #{node['omnibus']['build_dir']}; \
+    bash -l -c 'test -f ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh && \
+    . ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh; \
+    cd #{node['omnibus']['build_dir']}; \
     bundle install > #{logdir}/#{node['hostname']}-bundle-install.log 2>&1'
     EOF
 end
@@ -13,7 +15,9 @@ execute 'build project' do
   cwd node['omnibus']['build_dir']
   command <<-EOF.gsub(/\s+/, ' ').strip!
     sudo -i -u #{node['omnibus']['build_user']} \
-    bash -l -c 'cd #{node['omnibus']['build_dir']}; \
+    bash -l -c 'test -f ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh && \
+    . ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh; \
+    cd #{node['omnibus']['build_dir']}; \
     bundle exec omnibus build #{node['omnibus']['build_project']} > #{logdir}/#{node['hostname']}-build-project.log 2>&1'
     EOF
 end

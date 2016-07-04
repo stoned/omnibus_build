d9265cd7e949ef388d7d695ae01d80daef994932
Stoned Elipot <stoned.elipot@gmail.com>
2016-07-04 09:54:36 +0200

    Configure bundle for omnibus kitchen-based build environment
    Avoid using bundle's .bundle/config and install Gems in ~/.bundle
    so that omnibus can be used seamlessly "inside" and "outside"
    VirtualBox Test Kitchen VMs, from the same, shared, project directory.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,5 @@
 logdir = node['omnibus']['build_dir'] + '/.kitchen/logs'
+env_gem = 'env BUNDLE_IGNORE_CONFIG=1 BUNDLE_PATH=$HOME/.bundle'
 
 execute 'bundle install' do
   cwd node['omnibus']['build_dir']
@@ -7,7 +8,7 @@ execute 'bundle install' do
     bash -l -c 'test -f ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh && \
     . ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh; \
     cd #{node['omnibus']['build_dir']}; \
-    bundle install > #{logdir}/#{node['hostname']}-bundle-install.log 2>&1'
+    #{env_gem} bundle install > #{logdir}/#{node['hostname']}-bundle-install.log 2>&1'
     EOF
 end
 
@@ -18,6 +19,6 @@ execute 'build project' do
     bash -l -c 'test -f ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh && \
     . ~#{node['omnibus']['build_user']}/load-omnibus-toolchain.sh; \
     cd #{node['omnibus']['build_dir']}; \
-    bundle exec omnibus build #{node['omnibus']['build_project']} > #{logdir}/#{node['hostname']}-build-project.log 2>&1'
+    #{env_gem} bundle exec omnibus build #{node['omnibus']['build_project']} > #{logdir}/#{node['hostname']}-build-project.log 2>&1'
     EOF
 end
